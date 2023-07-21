2fa2fb11cf1ed7c3bb975f6d61dfe5add024dbc7
EC2 Default User <ec2-user@ip-172-31-6-61.ap-northeast-1.compute.internal>
2023-07-21 12:59:23 +0000

    admin edit

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,17 +1,14 @@
-class Public::UsersController < ApplicationController
+class Admin::UsersController < ApplicationController
   def index
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
-    @favorites = Favorite.where(user_id: @user.id).all
-
   end
-
+  
   def edit
     @user = User.find(params[:id])
   end
-  
   
 end
