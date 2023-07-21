ba835f2c47d3f1aaf0e039766b39726bb590b932
EC2 Default User <ec2-user@ip-172-31-6-61.ap-northeast-1.compute.internal>
2023-07-21 12:59:23 +0000

    admin edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
 
+ # 管理者用
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  namespace :admin do
+    resources :users, only: [:index, :show, :update, :destroy, :edit]
+    resources :recipe_comments, only: [:destroy]
+  end
+
   # ユーザー用
-  # URL /users/sign_in ...
   devise_for :users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
-  }
-
-  # 管理者用
-  # URL /admin/sign_in ...
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-    sessions: "admin/sessions"
   }
 
   scope module: 'public' do
@@ -20,10 +23,15 @@ Rails.application.routes.draw do
       resource :favorites, only: [:index, :create, :destroy]
       resources :recipe_comments, only: [:create, :destroy]
     end
-    resources :users
+    resources :users do
+      member do
+        get :favorites
+      end
+    end
     get '/search', to: 'searches#search'
-  
   end
+
+
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
