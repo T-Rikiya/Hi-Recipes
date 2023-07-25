94b87d1b99af2046d4440508ae796197a519c6a1
EC2 Default User <ec2-user@ip-172-31-6-61.ap-northeast-1.compute.internal>
2023-07-25 12:36:30 +0000

    デプロイ

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,8 @@
 class Public::HomesController < ApplicationController
   def top
-    @random = Recipe.order("RANDOM()").limit(5)
+    # @random = Recipe.order("RANDOM()").limit(5)
+    rand = Rails.env.production? ? "RANDOM()" : "rand()"
+    @random = Recipe.order(rand).limit(5)
   end
 
   def about
