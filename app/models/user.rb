66ff7fb2000085f16c1763d2192ff1cb33012795
EC2 Default User <ec2-user@ip-172-31-6-61.ap-northeast-1.compute.internal>
2023-07-25 11:04:13 +0000

    検索バー修正中

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,17 +23,7 @@ class User < ApplicationRecord
 
   
   def self.looks(search, word)
-    if search == "perfect_match"
-      @user = User.where("name LIKE?", "#{word}")
-    elsif search == "forward_match"
-      @user = User.where("name LIKE?","#{word}%")
-    elsif search == "backward_match"
-      @user = User.where("name LIKE?","%#{word}")
-    elsif search == "partial_match"
-      @user = User.where("name LIKE?","%#{word}%")
-    else
-      @user = User.all
-    end
+    @user = User.where("name LIKE?","%#{word}%")
   end
   
 end
