32704252bfd369d53f361b13d3bae3b1ddf324bf
EC2 Default User <ec2-user@ip-172-31-6-61.ap-northeast-1.compute.internal>
2023-07-27 11:29:37 +0000

    レイアウト

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create(
+  email:"admin@example.com", password:"password"
+)
+
+User.create([
+  { name:"料理一郎", email:"mail1@example.com", password:"password" }
+])
+
+Recipe.create([
+  { user_id: 1, title:"チキンカレー", body:"美味しい", instruction:"野菜を切る", ingredient:"鶏もも",
+  image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.png"), filename:"sample-post1.png")}
+])
